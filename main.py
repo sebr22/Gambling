4b46f67c6b2641b8ac179ea1f2e210193a5a6894
sebr22 <65369608+sebr22@users.noreply.github.com>
2023-05-19 00:33:10 +0000

    init

--- main.py
+++ main.py
@@ -3,155 +3,198 @@ from players import Player
 from players import BasicStrategyPlayer
 from players import CardCounter
 from players import RandomPlayer
+from players import MITCardCounter
+import csv
+import glob
+
+file = "output/"+str(len(glob.glob('./output/*')))+".csv"
+fp = open(file, 'x')
+fp.close()
+
+with open(file, mode='a') as list:
+    data = csv.writer(list)
+    data.writerow(["Dealer Up", "Drawn Cards", "Num of Hits", "Your End Total", "Dealer's Face Down", "Dealer Drawn Cards", "Dealer Num Hits", "Dealer End Total", "Did YOU Win"])
+
 
 class Deck:
-    def __init__(self):
-        self.cardList = []
-        self.drawnCards = []
 
-    def create_deck(self):
-        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        numbers = [str(x) for x in range(2, 11)] + ["Jack", "Queen", "King", "Ace"]
-        for suit in suits:
-            for number in numbers:
-                self.cardList.append(Card(suit, number))
-        random.shuffle(self.cardList)
-        return self.cardList
+  def __init__(self):
+    self.cardList = []
+    self.drawnCards = []
 
-    def __str__(self):
-        ret = ""
-        for x in self.cardList:
-            ret += x.__str__() + "\n"
-        return ret
+  def create_deck(self):
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    numbers = [str(x) for x in range(2, 11)] + ["Jack", "Queen", "King", "Ace"]
+    for suit in suits:
+      for number in numbers:
+        self.cardList.append(Card(suit, number))
+    random.shuffle(self.cardList)
+    return self.cardList
 
-    def draw(self):
-        card = self.cardList[0]
-        self.cardList.pop(0)
-        self.drawnCards.append(card)
-        return card
+  def __str__(self):
+    ret = ""
+    for x in self.cardList:
+      ret += x.__str__() + "\n"
+    return ret
+
+  def arr(self):
+    ret = []
+    for i in self.drawnCards:
+      ret.append(i.__str__())
+    return ret
+
+  def draw(self):
+    card = self.cardList[0]
+    self.cardList.pop(0)
+    self.drawnCards.append(card)
+    return card
 
 
 class Card:
-    def __init__(self, s, number):
-        self.suit = s
-        self.num = number
 
-    def __str__(self):
-        return str(self.num) + " of " + self.suit
+  def __init__(self, s, number):
+    self.suit = s
+    self.num = number
 
-    def __eq__(self, other):
-        return self.num == other.num
+  def __str__(self):
+    return str(self.num) + " of " + self.suit
 
-    def val(self):
-        if self.num.isdigit():
-            return [int(self.num), int(self.num)]
-        elif self.num == "Ace":
-            return [1, 11]
-        else:
-            return [10, 10]
+  def __eq__(self, other):
+    return self.num == other.num
+
+  def val(self):
+    if self.num.isdigit():
+      return [int(self.num), int(self.num)]
+    elif self.num == "Ace":
+      return [1, 11]
+    else:
+      return [10, 10]
 
 
 class Dealer:
-    def aces(self, card):
-        if card[0] == card[1]:
-            return card[0]
-        else:
-            return card[0]
 
-    def __init__(self, deck):
-        self.deck = deck
-        self.up = self.deck.draw()
-        self.count = self.aces(self.up.val())
+  def aces(self, card):
+    if card[0] == card[1]:
+      return card[0]
+    else:
+      return card[0]
 
-    def play(self):
-        while self.count <= 16:
-            self.count += int(self.aces(self.deck.draw().val()))
-            print("Dealer Hit")
-        if self.count > 21:
-            print("Dealer Bust")
-        else:
-            print("Dealer Stand")
-            self.stand = True
+  def __init__(self, deck):
+    self.deck = deck
+    self.up = self.deck.draw()
+    self.count = self.aces(self.up.val())
+    self.drawn = []
+    self.hits = 0
+
+  def play(self):
+    self.down = self.deck.draw()
+    while self.count <= 16:
+      new_card = self.deck.draw()
+      self.count += int(self.aces(new_card.val()))
+      self.drawn.append(new_card.__str__())
+      self.hits += 1
+      print("Dealer Hit")
+    if self.count > 21:
+      print("Dealer Bust")
+    else:
+      print("Dealer Stand")
+      self.stand = True
 
 
 class Tournament:
-    def __init__(self, players, rounds):
-        self.players = players
-        self.rounds = rounds
-        self.win_rates = {player.name: 0 for player in players}
 
-    def play_round(self):
-      deck = Deck()
-      deck.create_deck()
+  def __init__(self, players, rounds):
+    self.players = players
+    self.rounds = rounds
+    self.win_rates = {player.name: 0 for player in players}
 
-      dealer = Dealer(deck)
+  def play_round(self):
+    deck = Deck()
+    deck.create_deck()
 
-      # Update count for all players when dealer's up card is revealed
-      for p in range(len(self.players)):
-          if isinstance(self.players[p], CardCounter):
-            self.players[p].update_count(dealer.up)
+    dealer = Dealer(deck)
 
-      for p in range(len(self.players)):
-          card1 = deck.draw()
-          card2 = deck.draw()
+    # Update count for all players when dealer's up card is revealed
+    # for p in range(len(self.players)):
+    #     if isinstance(self.players[p], CardCounter):
+    #       self.players[p].update_count(dealer.up)
 
-          # Update count for all players when player's initial cards are dealt
-          if isinstance(self.players[p], CardCounter):
-            self.players[p].update_count(card1)
-            self.players[p].update_count(card2)
+    for p in range(len(self.players)):
+      card1 = deck.draw()
+      card2 = deck.draw()
 
-          # Allow player to make a bet
-          bet = min(100, self.players[p].chips)
-          self.players[p].bet(bet)
+      # Update count for all players when player's initial cards are dealt
+      # if isinstance(self.players[p], CardCounter):
+      #   self.players[p].update_count(card1)
+      #   self.players[p].update_count(card2)
 
-          # Initialize player's hand
-          self.players[p].init(card1, card2)
+      # Allow player to make a bet
+      bet = min(100, self.players[p].chips)
+      self.players[p].bet(bet)
 
-      for p in range(len(self.players)):
-          # Allow player to play their hand
-          self.players[p].play(deck, dealer)
-          print("\n")
+      # Initialize player's hand
+      self.players[p].init(card1, card2)
 
-      # Allow dealer to play their hand
-      dealer.play()
+    for p in range(len(self.players)):
+      # Allow player to play their hand
+      self.players[p].play(deck, dealer)
       print("\n")
 
-      # Determine outcome of round for each player
-      for p in range(len(self.players)):
-          if ((self.players[p].hands[0] > dealer.count) or (dealer.count > 21)) and (self.players[p].hands[0] <= 21):
-              print(self.players[p].name + " beat the dealer with a count of " + str(self.players[p].hands[0]))
-              # Player wins and receives double their bet
-              winnings = 2 * self.players[p].current_bet
-              self.players[p].chips += winnings
+    # Allow dealer to play their hand
+    dealer.play()
+    print("\n")
 
-              # Update win rate for player
-              self.win_rates[self.players[p].name] += 1 / self.rounds
-          else:
-              print(self.players[p].name + " lost to the dealer with a count of " + str(self.players[p].hands[0]))
-              # Player loses and loses their bet
-              loss = -1 * self.players[p].current_bet
-              self.players[p].chips += loss
+    # Determine outcome of round for each player
+    for p in range(len(self.players)):
+      self.win = False
+      if ((self.players[p].hands[0] > dealer.count) or
+          (dealer.count > 21)) and (self.players[p].hands[0] <= 21):
+        print(self.players[p].name + " beat the dealer with a count of " +
+              str(self.players[p].hands[0]))
+        self.win = True
+        # Player wins and receives double their bet
+        winnings = 2 * self.players[p].current_bet
+        self.players[p].chips += winnings
 
-    def play_tournament(self):
-      for round_num in range(1, self.rounds+1):
-          print(f"Round {round_num}")
-          print("--------")
-          
-          # Play a round of the tournament
-          self.play_round()
-
-      # Determine winner of tournament
-      max_chips = max([p.chips for p in self.players])
-      winner = next(p for p in self.players if p.chips == max_chips)
-      print(f"{winner.name} has won the tournament with {winner.chips} chips")
-
-    def display_win_rates(self):
-        print("\nWin Rates:")
-        for player_name, win_rate in self.win_rates.items():
-            print(f"{player_name}: {win_rate:.2f}")
+        # Update win rate for player
+        self.win_rates[self.players[p].name] += 1 / self.rounds
+      else:
+        print(self.players[p].name + " lost to the dealer with a count of " +
+              str(self.players[p].hands[0]))
+        # Player loses and loses their bet
+        loss = -1 * self.players[p].current_bet
+        self.players[p].chips += loss
+      with open(file, mode='a') as list:
+        data = csv.writer(list)
+        data.writerow([dealer.up, self.players[p].drawnatm, self.players[p].hits, self.players[p].hands[0], str(dealer.down), dealer.drawn, dealer.hits, dealer.count, self.win])
 
 
-players = [Player("Player"), BasicStrategyPlayer("Basic"), CardCounter("Card Counter"), RandomPlayer("Rando")]
-tournament = Tournament(players, 5)
+  def play_tournament(self):
+    for round_num in range(1, self.rounds + 1):
+      print(f"Round {round_num}")
+      print("--------")
+
+      # Play a round of the tournament
+      self.play_round()
+
+    # Determine winner of tournament
+    max_chips = max([p.chips for p in self.players])
+    winner = next(p for p in self.players if p.chips == max_chips)
+    print(f"{winner.name} has won the tournament with {winner.chips} chips")
+
+  def display_win_rates(self):
+    print("\nWin Rates:")
+    for player_name, win_rate in self.win_rates.items():
+      print(f"{player_name}: {win_rate:.2f}")
+
+
+players = [
+  Player("Player"),
+  BasicStrategyPlayer("Basic"),
+  CardCounter("Card Counter"),
+  RandomPlayer("Rando"),
+  MITCardCounter("MIT")
+]
+tournament = Tournament(players, 100)
 tournament.play_tournament()
 tournament.display_win_rates()
--- players.py
+++ players.py
@@ -2,58 +2,70 @@ import random
 
 
 class Player:
-    def __init__(self, name):
-        self.name = name
-        # Give each player an initial chip count of 1000
-        self.chips = 1000
 
-    def chooser(self, card):
-        if card[0] == card[1]:
-            return card[0]
-        else:
-            return card[0]
+  def __init__(self, name):
+    self.name = name
+    # Give each player an initial chip count of 1000
+    self.chips = 1000
+    self.hits = 0
+    self.drawnatm = []
 
-    def init(self, card1, card2):
-        # self.hands = [0, 0]
-        self.stand = False
-        if card1 == card2:
-            self.hands = [
-                int(self.chooser(card1.val())),
-                int(self.chooser(card2.val())),
-            ]
-        else:
-            self.hands = [
-                int(self.chooser(card1.val())) + int(self.chooser(card2.val()))
-            ]
+  def chooser(self, card):
+    if card[0] == card[1]:
+      return card[0]
+    else:
+      return card[0]
 
-    def play(self, deck, dealer):
-        # print("the dealers card is: " + dealer.up.__str__())
-        for x in range(len(self.hands)):
-            while self.hands[x] <= 16:
-                print(self.name + " Hit " + str(self.hands[x]))
-                self.hands[x] += int(self.chooser(deck.draw().val()))
-            if self.hands[x] > 21:
-                print(self.name + " Bust " + str(self.hands[x]))
-            else:
-                print(self.name + " Stand " + str(self.hands[x]))
-                self.stand = True
+  def init(self, card1, card2):
+    # self.hands = [0, 0]
+    self.stand = False
+    if card1 == card2:
+      self.hands = [
+        int(self.chooser(card1.val())),
+        int(self.chooser(card2.val())),
+      ]
+    else:
+      self.hands = [
+        int(self.chooser(card1.val())) + int(self.chooser(card2.val()))
+      ]
 
-    def bet(self, amount):
-        # Deduct bet amount from player's chips and store current bet amount
-        if amount > 0 and amount <=self.chips:
-            print(f"{self.name} bets {amount}")
-            self.current_bet = amount
-            self.chips -= amount
+  def play(self, deck, dealer):
+    self.drawnatm = deck.arr()
+    # print("the dealers card is: " + dealer.up.__str__())
+    for x in range(len(self.hands)):
+      while self.hands[x] <= 16:
+        print(self.name + " Hit " + str(self.hands[x]))
+        self.hands[x] += int(self.chooser(deck.draw().val()))
+        self.hits += 1
+      if self.hands[x] > 21:
+        print(self.name + " Bust " + str(self.hands[x]))
+      else:
+        print(self.name + " Stand " + str(self.hands[x]))
+        self.stand = True
+    
+# --------------------------------------------------------
+
+  def bet(self, amount):
+    # Deduct bet amount from player's chips and store current bet amount
+    if amount > 0 and amount <= self.chips:
+      print(f"{self.name} bets {amount}")
+      self.current_bet = amount
+      self.chips -= amount
+
+
+# --------------------------------------------------------
 
 
 class RandomPlayer(Player):
 
   def play(self, deck, dealer):
+    self.drawnatm = deck.arr()
     for x in range(len(self.hands)):
       while self.hands[x] <= 16:
         if random.choice([True, False]):
           print(self.name + " Hit " + str(self.hands[x]))
           self.hands[x] += int(self.chooser(deck.draw().val()))
+          self.hits += 1
         else:
           break
       if self.hands[x] > 21:
@@ -61,11 +73,12 @@ class RandomPlayer(Player):
       else:
         print(self.name + " Stand " + str(self.hands[x]))
         self.stand = True
-
+    
 
 class BasicStrategyPlayer(Player):
 
   def play(self, deck, dealer):
+    self.drawnatm = deck.arr()
     dealer_upcard = int(self.chooser(dealer.up.val()))
     for x in range(len(self.hands)):
       while self.hands[x] <= 16:
@@ -83,12 +96,13 @@ class BasicStrategyPlayer(Player):
             break
         print(self.name + " Hit " + str(self.hands[x]))
         self.hands[x] += int(self.chooser(deck.draw().val()))
+        self.hits += 1
       if self.hands[x] > 21:
         print(self.name + " Bust " + str(self.hands[x]))
       else:
         print(self.name + " Stand " + str(self.hands[x]))
         self.stand = True
-
+    
 
 class CardCounter(Player):
 
@@ -97,13 +111,15 @@ class CardCounter(Player):
     self.count = 0
 
   def play(self, deck, dealer):
+    self.drawnatm = deck.arr()
     for x in range(len(self.hands)):
       while self.hands[x] <= 16:
         if self.count >= 2:
           print(self.name + " Hit " + str(self.hands[x]))
           card = deck.draw()
-          self.update_count(card)
+          self.update_count(deck)
           self.hands[x] += int(self.chooser(card.val()))
+          self.hits += 1
         else:
           break
       if self.hands[x] > 21:
@@ -117,3 +133,50 @@ class CardCounter(Player):
       self.count += 1
     elif card.num in ['10', 'Jack', 'Queen', 'King', 'Ace']:
       self.count -= 1
+
+
+class MITCardCounter(Player):
+
+  def __init__(self, name):
+    super().__init__(name)
+    self.count = 0
+    self.true_count = 0
+    self.min_bet = 10
+    self.max_bet = 100
+
+  def play(self, deck, dealer):
+    self.drawnatm = deck.arr()
+    for x in range(len(self.hands)):
+      while self.hands[x] <= 16:
+        if self.true_count >= 2:
+          print(self.name + " Hit " + str(self.hands[x]))
+          card = deck.draw()
+          self.update_count(deck)
+          self.hands[x] += int(self.chooser(card.val()))
+          self.hits += 1
+        else:
+          break
+      if self.hands[x] > 21:
+        print(self.name + " Bust " + str(self.hands[x]))
+      else:
+        print(self.name + " Stand " + str(self.hands[x]))
+        self.stand = True
+
+  def update_count(self, deck):
+    self.count = 0
+    for card in deck.drawnCards:
+      if card.num in ['2', '3', '4', '5', '6']:
+        self.count += 1
+      elif card.num in ['10', 'Jack', 'Queen', 'King', 'Ace']:
+        self.count -= 1
+      # Calculate the true count by dividing the running count by the number of decks remaining
+      decks_remaining = (len(deck.cardList) + len(deck.drawnCards)) / 52
+      self.true_count = self.count / decks_remaining
+
+  def bet(self, amount):
+    # Vary the bet size based on the true count
+    if self.true_count >= 2:
+      bet_amount = min(self.max_bet, amount)
+    else:
+      bet_amount = min(self.min_bet, amount)
+    super().bet(bet_amount)
